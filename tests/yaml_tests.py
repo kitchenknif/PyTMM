0e35348cf0675fcca8d0f648ff20e968ba155e01
kitchenknif <pavel.a.dmitriev@gmail.com>
2015-07-17 20:59:40 +0300

    GPL stuff added 2

--- tests/yaml_tests.py
+++ tests/yaml_tests.py
@@ -1,6 +1,24 @@
+#    This file is part of PyTMM.
+#
+#    PyTMM is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    PyTMM is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#   Copyright 2014-2015 Pavel Dmitriev <pavel.a.dmitriev@gmail.com>
+
 from refractiveIndex import *
 
-catalog = RefractiveIndex("../RefractiveIndex")
+# catalog = RefractiveIndex("../RefractiveIndex")
 mat = catalog.getMaterial('other', 'pmma_resists', 'Microchem495')
 print(mat.getRefractiveIndex(500))
 #f = open('Hass.yml')
